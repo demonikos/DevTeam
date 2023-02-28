5b6cec5dc00bf1a640e333064bde5cac467736be
Shprinher <119606175+Shprinher@users.noreply.github.com>
2023-02-28 10:00:47 +0200

    saved the fix
    shprinher

--- src/js/restaurant-hero-slider.js
+++ src/js/restaurant-hero-slider.js
@@ -23,7 +23,7 @@ if (viewportWidth >= 1440) {
 } else if (viewportWidth >= 768) {
   buttonRight.addEventListener('click', function () {
     offset = offset + 456;
-    if (offset > 3312) {
+    if (offset > 2180) {
       offset = 0;
     }
     photoSlider.style.right = offset + 'px';
@@ -38,7 +38,7 @@ if (viewportWidth >= 1440) {
 } else {
   buttonRight.addEventListener('click', function () {
     offset = offset + 270;
-    if (offset > 1593) {
+    if (offset > 1250) {
       offset = 0;
     }
     photoSlider.style.right = offset + 'px';
